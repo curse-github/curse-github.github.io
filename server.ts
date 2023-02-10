22c3d0e54e7a1863d896a9773b1b7ed757ae2723
Campbell Simpson <curse@simpsoncentral.com>
2023-02-10 03:06:19 -0600

    Name animation effect

---
+++ server.ts
@@ -0,0 +1,21 @@
+const express = require("express");
+import * as fs from "fs";
+
+var app:any = express();
+var server:any = app.listen(80, function () {
+    var host:string = server.address().address;
+    var port:number = server.address().port;
+    console.clear();
+    console.log("Public web execution page is running at http://localhost:" + port);
+});
+var pages:{[key:string]:any} = {
+    "/index.html":(req:any, res:any, send:any) => {send();},
+    "/index.css":(req:any, res:any, send:any) => {send();},
+    "/index.js":(req:any, res:any, send:any) => {send();},
+}
+Object.keys(pages).forEach((i) => {
+    app.get(i, (req:any, res:any) => {
+        pages[i](req, res, () => { res.sendFile(__dirname + i, "utf8"); });
+    });
+});
+["/favicon.ico"].forEach((i) => { app.get(i, (req:any, res:any) => { res.sendFile(__dirname + i, "utf8"); }); });
