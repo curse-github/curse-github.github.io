df536c5ce82fc592832134a1b042bbb00ee8e238
Campbell Simpson <curse@simpsoncentral.com>
2023-02-10 16:11:11 -0600

    fixed navbar

--- server.ts
+++ server.ts
@@ -2,12 +2,6 @@ const express = require("express");
 import * as fs from "fs";
 
 var app:any = express();
-var server:any = app.listen(80, function () {
-    var host:string = server.address().address;
-    var port:number = server.address().port;
-    console.clear();
-    console.log("Public web execution page is running at http://localhost:" + port);
-});
 var pages:{[key:string]:any} = {
     "/index.html":(req:any, res:any, send:any) => {send();},
     "/index.css":(req:any, res:any, send:any) => {send();},
@@ -20,3 +14,10 @@ Object.keys(pages).forEach((i) => {
     });
 });
 ["/favicon.ico"].forEach((i) => { app.get(i, (req:any, res:any) => { res.sendFile(__dirname + i, "utf8"); }); });
+app.get("*", (req:any, res:any) => { res.redirect("/index.html"); });
+var server:any = app.listen(80, function () {
+    var host:string = server.address().address;
+    var port:number = server.address().port;
+    console.clear();
+    console.log("Public web execution page is running at http://localhost:" + port);
+});
