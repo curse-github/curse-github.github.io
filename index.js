123b942eb1a9c6d1d7200c0b69379bbbf495ffb9
Campbell Simpson <curse@simpsoncentral.com>
2023-02-10 23:55:13 -0600

    added github cards

---
+++ index.js
@@ -0,0 +1,51 @@
+function fetchJson(url, callback) {
+    fetch(url)
+    .then((response) => response.json())
+    .then(callback);
+}
+async function fetchJsonPromise(url) {
+    return new Promise((resolve) => {
+        fetchJson(url,(json) => {
+            resolve(json);
+        })
+    });
+}
+function append(parent,type,content,attributes) {
+	var thing;
+	if (type != null) { thing = document.createElement(type); } else { document.createElement("div"); }
+	if (content != null) { thing.innerHTML = content; }
+	Object.keys(attributes).forEach((i) => {
+		thing.setAttribute(i, attributes[i]);
+	});
+	parent.appendChild(thing);
+	return thing;
+}
+
+async function run() {
+    var json = await fetchJsonPromise("https://api.github.com/users/curse-github/repos");
+    for(var i = 0; i < json.length; i++) {
+        var card = append(document.getElementById("cardList"),"card","",{});
+        
+        var header = append(card,"header","",{});
+        append(header,"a",json[i].name,{
+            target:"_blank",
+            href:json[i].html_url,
+            style:"text-decoration: none;"
+        });
+
+        var inner  = append(card,"inner" ,"",{});
+        append(inner,"img","",{
+            src:"https://raw.githubusercontent.com/" + json[i].full_name + "/" + json[i].default_branch + "/Preview.png",
+            alt:"github-logo.png"
+        });
+        
+        var footer = append(card,"footer","",{});
+        append(footer,"input","",{
+            type:"button",
+            value:"v",
+            onclick:"var parent = this.parentNode.parentNode; parent.setAttribute('show',parent.getAttribute('show') != 'true'); this.value = ((this.value == 'v') ? '^' : 'v');",
+        });
+        append(footer,"hide",json[i].description,{});
+    }
+}
+run();
--- server.ts
+++ server.ts
@@ -6,14 +6,19 @@ var pages:{[key:string]:any} = {
     "/index.html":(req:any, res:any, send:any) => {send();},
     "/index.css":(req:any, res:any, send:any) => {send();},
     "/index.js":(req:any, res:any, send:any) => {send();},
-    "/letterAnim.css":(req:any, res:any, send:any) => {send();}
+    "/letterAnimHover.css":(req:any, res:any, send:any) => {send();},
+    "/letterAnimOpen.css":(req:any, res:any, send:any) => {send();},
+    "/githubCards.css":(req:any, res:any, send:any) => {send();},
+
+    "/favicon.ico":(req:any, res:any, send:any) => {send();},
+    "/github-logo.png":(req:any, res:any, send:any) => {send();}
 }
 Object.keys(pages).forEach((i) => {
     app.get(i, (req:any, res:any) => {
         pages[i](req, res, () => { res.sendFile(__dirname + i, "utf8"); });
     });
 });
-["/favicon.ico"].forEach((i) => { app.get(i, (req:any, res:any) => { res.sendFile(__dirname + i, "utf8"); }); });
+[,""].forEach((i) => { app.get(i, (req:any, res:any) => { res.sendFile(__dirname + i, "utf8"); }); });
 app.get("*", (req:any, res:any) => { res.redirect("/index.html"); });
 var server:any = app.listen(80, function () {
     var host:string = server.address().address;
