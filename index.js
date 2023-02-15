bbba731d7af3da830e5b83432e32f151c20a2b0f
Campbell Simpson <curse@simpsoncentral.com>
2023-02-15 01:46:25 -0600

    dropdown fix, margin fix, link styling

--- index.js
+++ index.js
@@ -20,71 +20,78 @@ function append(parent,type,content,attributes) {
 	parent.appendChild(thing);
 	return thing;
 }
-function appendCard(parent, name, html_url, imagelink, hpLink, description) {
-    var card = append(parent,"div","",{
-        class:"gitCard",
-        onclick:"this.setAttribute('show',this.getAttribute('show') != 'true');"
-    });
-    
+
+//#region github cards
+const USER = "curse-github";
+var cardList = null;
+
+function appendCard(parent) {
+    var cardParent = append(parent,"cardParent","",{});
+    var card   = append(cardParent,"gitCard","",{});
     var header = append(card,"header","",{});
+    var inner  = append(card,"inner" ,"",{});
+    var footer = append(card,"footer","",{});
+    return [header,inner,footer];
+
+}
+function appendGitCard(json) {
+    [header,inner,footer] = appendCard(cardList);
+    var name = json.name.split("-").join(" ").replace("curse github","curse-github")
     append(header,"a",name,{
-        target:"_blank",
-        href:html_url,
-        style:"text-decoration: none;"
+        style: "text-decoration: none;",
+        href: json.html_url, target: "_blank"
     });
 
-    var inner  = append(card,"inner" ,"",{});
+    inner.setAttribute("onclick","this.parentElement.setAttribute('show',this.parentElement.getAttribute('show') != 'true');")
     append(inner,"img","",{
-        src:imagelink,
-        onerror:"setAltImg(this);",
-        draggable:"false"
+        src: "https://raw.githubusercontent.com/" + json.full_name + "/" + json.default_branch + "/Preview.png",
+        onerror: "setAltImg(this);", draggable: "false"
     });
-    
-    var footer = append(card,"footer","",{});
+    var hpLink = json.homepage;
     if (hpLink != null && hpLink != "") {
         var link = append(footer,"a","",{
             class:"link-homepage",
-            href:hpLink
+            href:hpLink, target: "_blank"
         }); append(link,"img","",{
             class:"link-homepage",
             src:"link.png", draggable:"false"
         }); link.innerHTML += hpLink.replace("https://","");
     }
-    append(footer,"div",((description != null && description != "") ? "\"" + description + "\"" : ""),{});
-    return card;
-
+    append(footer,"div",((json.description != null && json.description != "") ? "\"" + json.description + "\"" : ""),{});
+    return [header,inner,footer];
 }
-//#region github cards
-async function run() {
-    const USER = "curse-github";
-    const cardList = document.querySelector("#cardList");
-    
-    var cardParent = append(cardList,"cardParent","",{});
-    var json = await fetchJsonPromise("https://api.github.com/users/" + USER);
-    var card = {
-        parent: cardParent,
-        name: json.name,
-        html_url: json.html_url,
-        imagelink: json.avatar_url,
-        hpLink: json.blog,
-        description: json.bio
-    };
-    var cardElement = appendCard(card.parent,card.name,card.html_url,card.imagelink,card.hpLink,card.description);
-    cardElement.children[1].children[0].setAttribute("alt","pfPic");
-    json = null;
+function appendUserCard(json) {
+    [header,inner,footer] = appendCard(cardList);
+    append(header,"a",json.name,{
+        style: "text-decoration: none;",
+        href: json.html_url, target: "_blank"
+    });
 
+    inner.setAttribute("onclick","this.parentElement.setAttribute('show',this.parentElement.getAttribute('show') != 'true');")
+    append(inner,"img","",{
+        src: json.avatar_url, draggable: "false",
+        onerror: "setAltImg(this);", alt:"pfPic"
+    });
+    var hpLink = json.homepage;
+    if (json.blog != null && json.blog != "") {
+        var link = append(footer,"a","",{
+            class:"link-homepage",
+            href:hpLink, target: "_blank"
+        }); append(link,"img","",{
+            class:"link-homepage",
+            src:"link.png", draggable:"false"
+        }); link.innerHTML += json.blog.replace("https://","");
+    }
+    //<a href = "mailto: abc@example.com">Send Email</a>
+    append(footer,"div",((json.bio != null && json.bio != "") ? "\"" + json.bio + "\"" : ""),{});
+    return [header,inner,footer];
+}
+async function run() {
+    cardList = await document.querySelector("#cardList");
+    appendUserCard(await fetchJsonPromise("https://api.github.com/users/" + USER));
     json = await fetchJsonPromise("https://api.github.com/users/" + USER + "/repos");
     for(var i = 0; i < json.length; i++) {
-        var cardParent = append(cardList,"cardParent","",{});
-        var card = {
-            parent: cardParent,
-            name: json[i].name.split("-").join(" ").replace("curse github","curse-github"),
-            html_url: json[i].html_url,
-            imagelink: "https://raw.githubusercontent.com/" + json[i].full_name + "/" + json[i].default_branch + "/Preview.png",
-            hpLink: json[i].homepage,
-            description: json[i].description
-        };
-        appendCard(card.parent,card.name,card.html_url,card.imagelink,card.hpLink,card.description);
+        appendGitCard(json[i]);
     }
 }
 function setAltImg(element) {
